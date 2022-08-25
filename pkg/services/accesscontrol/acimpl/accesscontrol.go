6e2486fc94030b0de04c68303d1f49140ab58291
Karl Persson <kalle.persson@grafana.com>
2022-08-25 16:20:21 +0200

    RBAC: Implement a scope checker generator

--- pkg/services/accesscontrol/acimpl/accesscontrol.go
+++ pkg/services/accesscontrol/acimpl/accesscontrol.go
@@ -3,6 +3,7 @@ package acimpl
 import (
 	"context"
 	"errors"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 
@@ -34,16 +35,12 @@ func (a *AccessControl) Evaluate(ctx context.Context, user *user.SignedInUser, e
 	defer timer.ObserveDuration()
 	metrics.MAccessEvaluationCount.Inc()
 
-	if user.Permissions == nil {
-		user.Permissions = map[int64]map[string][]string{}
-	}
-
-	if _, ok := user.Permissions[user.OrgID]; !ok {
+	if !verifyPermissions(user) {
 		permissions, err := a.service.GetUserPermissions(ctx, user, accesscontrol.Options{ReloadCache: true})
 		if err != nil {
 			return false, err
 		}
-		user.Permissions[user.OrgID] = accesscontrol.GroupScopesByAction(permissions)
+		user.Permissions = map[int64]map[string][]string{user.OrgID: accesscontrol.GroupScopesByAction(permissions)}
 	}
 
 	// Test evaluation without scope resolver first, this will prevent 403 for wildcard scopes when resource does not exist
@@ -62,10 +59,79 @@ func (a *AccessControl) Evaluate(ctx context.Context, user *user.SignedInUser, e
 	return resolvedEvaluator.Evaluate(user.Permissions[user.OrgID]), nil
 }
 
+type Checker func(scopes ...string) bool
+
+func (a *AccessControl) GenerateChecker(ctx context.Context, user *user.SignedInUser, prefixes []string, action string) Checker {
+	if !verifyPermissions(user) {
+		return func(scope ...string) bool { return false }
+	}
+
+	permissions, ok := user.Permissions[user.OrgID]
+	if !ok {
+		return func(scope ...string) bool { return false }
+	}
+
+	checkers := map[string]Checker{}
+	scopes, ok := permissions[action]
+	if !ok {
+		checkers[action] = func(scope ...string) bool { return false }
+	}
+
+	wildcards := generatePossibleWildcards(prefixes)
+	lookup := make(map[string]bool, len(scopes)-1)
+	for _, s := range scopes {
+		if wildcards.Contains(s) {
+			return func(scope ...string) bool { return true }
+		}
+		lookup[s] = true
+	}
+
+	return func(scopes ...string) bool {
+		for _, s := range scopes {
+			if lookup[s] {
+				return true
+			}
+		}
+		return false
+	}
+}
+
 func (a *AccessControl) RegisterScopeAttributeResolver(prefix string, resolver accesscontrol.ScopeAttributeResolver) {
 	a.resolvers.AddScopeAttributeResolver(prefix, resolver)
 }
 
 func (a *AccessControl) IsDisabled() bool {
 	return accesscontrol.IsDisabled(a.cfg)
+}
+
+func verifyPermissions(u *user.SignedInUser) bool {
+	if u.Permissions == nil {
+		return false
+	}
+	if _, ok := u.Permissions[u.OrgID]; !ok {
+		return false
+	}
+	return true
+}
+
+type Wildcards []string
+
+func (wildcards Wildcards) Contains(scope string) bool {
+	for _, wildcard := range wildcards {
+		if scope == wildcard {
+			return true
+		}
+	}
+	return false
+}
+
+func generatePossibleWildcards(prefixes []string) Wildcards {
+	wildcards := Wildcards{"*"}
+	for _, prefix := range prefixes {
+		parts := strings.Split(prefix, ":")
+		for _, p := range parts {
+			wildcards = append(wildcards, p+":*")
+		}
+	}
+	return wildcards
 }
